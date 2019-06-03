7210c01e82debef1a3d011a36bb75dd4e5b4a955
wolf637 <alexey.volkov@live.com>
2019-06-02 23:08:45 -0700

    Add partials and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   def index
-    @articles = Article.all 
+    @articles = Article.all
   end
 
   def new
@@ -36,6 +36,13 @@ class ArticlesController < ApplicationController
 
   def edit
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was succesfully deleted"
+    redirect_to articles_path
   end
 
   private
